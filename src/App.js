503e3f5b4efeca02e2ced97221498e0660993e3f
Aditya Khedekar <adityakhedekar98906@gmail.com>
2023-12-21 01:37:37 +0530

    created home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import ToolTip from "./Tooltip";
 
 function App() {
+  const handleClick = (e) => {};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="bg-slate-900 h-screen flex items-center justify-center text-white">
+        <div className="text-center">
+          <h1 className="text-orange-200 mb-4 font-bold text-4xl">
+            Coding Ninjas React Skill Test!!
+          </h1>
+          <p className="mb-24 font-bold text-2xl ">
+            Hover over the Button to see the ToolTip in your specified
+            direction!!
+          </p>
+
+          <div>
+            <button
+              onClick={handleClick}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Right
+            </button>
+            <button
+              onClick={handleClick}
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Left
+            </button>
+            <button
+              onClick={handleClick}
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Top
+            </button>
+            <button
+              onClick={handleClick}
+              className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Bottom
+            </button>
+          </div>
+        </div>
+
+        <ToolTip />
+      </div>
+    </>
   );
 }
 
---
+++ src/Tooltip.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function ToolTip() {
+  return (
+    <>
+      <div>
+        <button className="bg-violet-600 h-20 w-28 rounded-xl">
+          Hover Over Me!!
+        </button>
+      </div>
+    </>
+  );
+}
