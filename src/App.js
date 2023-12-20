5b04e9c8cefc8d91811cb5b046601487da7c5659
Aditya Khedekar <adityakhedekar98906@gmail.com>
2023-12-21 03:22:04 +0530

    added bg grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import ToolTip from "./Tooltip";
-
+import { useState } from "react";
 function App() {
-  const handleClick = (e) => {};
+  const [selectButton, setSelectButton] = useState("top");
+
+  const [pos, setPos] = useState("top");
+
+  const handleClick = (e) => {
+    setPos((prevPos) => {
+      return (prevPos = e.target.value);
+    });
+    setSelectButton(e.target.value);
+  };
+
   return (
     <>
-      <div className="bg-slate-900 h-screen flex items-center justify-center text-white">
+      <div className="bg-slate-900 h-screen flex items-center justify-center text-white absolute inset-0  w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px] ">
         <div className="text-center">
           <h1 className="text-orange-200 mb-4 font-bold text-4xl">
             Coding Ninjas React Skill Test!!
@@ -17,35 +27,45 @@ function App() {
           <div>
             <button
               onClick={handleClick}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              value="right"
+              classN="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              className={selectButton === "right" ? "selected" : ""}
             >
               Right
             </button>
             <button
               onClick={handleClick}
-              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+              value="left"
+              class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+              className={selectButton === "left" ? "selected" : ""}
             >
               Left
             </button>
             <button
               onClick={handleClick}
-              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+              value="top"
+              class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded "
+              className={selectButton === "top" ? "selected" : ""}
             >
               Top
             </button>
             <button
               onClick={handleClick}
-              className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+              value="bottom"
+              class="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+              className={selectButton === "bottom" ? "selected" : ""}
             >
               Bottom
             </button>
           </div>
         </div>
 
-        <ToolTip />
+        <ToolTip position={pos} />
       </div>
     </>
   );
 }
 
 export default App;
+
+
--- src/Tooltip.js
+++ src/Tooltip.js
@@ -1,12 +1,42 @@
 import React from "react";
+import { useState } from "react";
 
-export default function ToolTip() {
+export default function ToolTip({ position }) {
+  const [flag, setFlag] = useState(false);
+
+  const handleMouseOn = () => {
+    setFlag(true);
+  };
+
+  const handleMouseLeave = () => {
+    setFlag(false);
+  };
+
+  const visible = { display: flag ? "block" : "none" };
+
   return (
     <>
       <div>
-        <button className="bg-violet-600 h-20 w-28 rounded-xl">
+        <button
+          className="bg-violet-600 h-20 w-28 rounded-xl"
+          data-tooltip-target="tooltip-light"
+          data-tooltip-style="light"
+          type="button"
+          onMouseOver={handleMouseOn}
+          onMouseOut={handleMouseLeave}
+        >
           Hover Over Me!!
         </button>
+        <div
+          id="tooltip-light"
+          role="tooltip"
+          className={`${position}`}
+          style={visible}
+          class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 tooltip"
+        >
+          i am {position} tip
+          <div class="tooltip-arrow" data-popper-arrow></div>
+        </div>
       </div>
     </>
   );
